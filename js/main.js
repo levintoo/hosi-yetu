93312d8e834fd9db050569e7168e1ea5b3b40c2e
levintoo <tooklevin@gmail.com>
2021-07-05 11:34:19 +0300

    navbar menu icon

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
 document.getElementById("trademark-slang").innerHTML="Hi there";
 document.getElementById("trademark-text").innerHTML="Hi there this is no lorem";
+
+
+let toggleButton= document.getElementById("menu-toggle");
+let links = document.querySelector(".links");
+
+
+toggleButton.style.transform ="rotate(0deg)";
+toggleButton.style.transition ="1s";
+
+
+toggleButton.addEventListener ("click", function(){
+    links.classList.toggle("flex-column");
+    links.classList.toggle("show-links");
+    toggleButton.style.transform ="rotate(240deg)";
+    toggleButton.style.transition ="1s";
+})
